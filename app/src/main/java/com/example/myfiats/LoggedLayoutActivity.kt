929ed9f648f7da4d34cc7ae79abb37b466a3adcd
mikosovsky <mikolaj.dawczyk@gmail.com>
2023-05-26 20:07:55 +0200

    Add apikeys.xml to .gitignore

--- app/src/main/java/com/example/myfiats/LoggedLayoutActivity.kt
+++ app/src/main/java/com/example/myfiats/LoggedLayoutActivity.kt
@@ -2,10 +2,17 @@ package com.example.myfiats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.net.URL
 
 class LoggedLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.logged_layout)
+    }
+
+    private fun fetchCurrencyData(): Thread {
+        return Thread {
+            val url = URL("")
+        }
     }
 }
