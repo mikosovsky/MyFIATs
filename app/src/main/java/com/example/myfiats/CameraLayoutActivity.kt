207db3fc4c6ff30080e23c1a0442684773d4198e
mikosovsky <mikolaj.dawczyk@gmail.com>
2023-06-11 18:06:34 +0200

    Prepare button and layout to camera

---
+++ app/src/main/java/com/example/myfiats/CameraLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myfiats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CameraLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.camera_layout)
+    }
+}
